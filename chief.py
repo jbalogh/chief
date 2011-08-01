154b3f73c10714727b9a85737dfa330b3087e50c
Jeremy Orem <jeremy.orem@gmail.com>
2011-08-01 16:32:17 -0700

    Convert to flask app.

--- chief.py
+++ chief.py
@@ -1,13 +1,15 @@
 import json
 import os
 import subprocess
-import urlparse
 
 import redis as redislib
+from flask import Flask, Response, request, render_template
 
 import settings
 
-deploy = os.path.join(settings.ZAMBONI_DIR, 'scripts/deploy.py')
+
+app = Flask(__name__)
+deploy = os.path.join(settings.ZAMBONI_DIR, 'scripts/update/update.py')
 
 os.environ['PYTHONUNBUFFERED'] = 'go time'
 
@@ -29,59 +31,27 @@ def do_update(zamboni_tag, vendor_tag, who):
     yield 'Updating! zamboni: %s -- vendor: %s<br>' % (zamboni_tag, vendor_tag)
 
     output = open(os.path.join(settings.OUTPUT_DIR, zamboni_tag), 'a')
-    run('start_update:%s,%s' % (zamboni_tag, vendor_tag), output)
-
+    run('pre_update:%s,%s' % (zamboni_tag, vendor_tag), output)
     pub('PUSH')
     yield 'We have the new code!<br>'
 
-    run('update_amo', output)
+    run('update', output)
+    pub('UPDATE')
+    yield "Code has been updated locally!<br>"
 
+    run('deploy', output)
     pub('DONE')
     yield 'All done!'
 
 
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    if env['REQUEST_METHOD'] == 'POST':
-        post = dict(urlparse.parse_qsl(env['wsgi.input'].read()))
+@app.route("/")
+def index():
+    if request.method == 'POST':
+        post = request.form
         assert sorted(post.keys()) == ['password', 'vendor', 'who', 'zamboni']
         assert post['password'] == settings.PASSWORD
-        return do_update(post['zamboni'], post['vendor'], post['who'])
+        return Response(
+                do_update(post['zamboni'], post['vendor'], post['who']),
+                direct_passthrough=True, mimetype='text/plain')
 
-    return html
-
-
-# So fancy.
-html = """
-<title>CHIEF</title>
-<style>
-button {
-  background: red;
-  font-size: 32px;
-  font-family: monospace;
-  margin: 1em 0 0;
-  padding: .2em;
-  border: none;
-  border-radius: 6px;
-  box-shadow: inset 0 0 .3em;
-  text-shadow: 0 0 .3em #fff;
-  cursor: pointer;
-}
-button:hover {
-  font-weight: bold;
-  box-shadow: inset 0 0 .3em,
-                    0 0 1em red;
-}
-button:active {
-  color: white;
-}
-</style>
-<form method="post" action="">
-  <input name="zamboni" placeholder="zamboni tag">
-  <input name="vendor" placeholder="vendor tag">
-  <input name="password" type="password" placeholder="secret">
-  <input name="who" placeholder="identify yourself">
-  <br>
-  <button>BIG RED BUTTON</button>
-</form>
-"""
+    return render_template("index.html")
