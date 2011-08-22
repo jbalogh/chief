2b5af25d1d8d9f7608916d13bdcde6902bfb8648
Jeff Balogh <jbalogh@mozilla.com>
2011-08-22 11:49:55 -0700

    use check_call so we stop on errors (bug 680568)

--- chief.py
+++ chief.py
@@ -16,9 +16,8 @@ os.environ['PYTHONUNBUFFERED'] = 'go time'
 
 
 def run(task, output):
-    proc = subprocess.Popen('commander %s %s' % (deploy, task),
-                            shell=True, stdout=output, stderr=output)
-    proc.communicate()
+    subprocess.check_call('commander %s %s' % (deploy, task),
+                          shell=True, stdout=output, stderr=output)
 
 
 def do_update(zamboni_tag, vendor_tag, who):
@@ -32,7 +31,7 @@ def do_update(zamboni_tag, vendor_tag, who):
     yield 'Updating! zamboni: %s -- vendor: %s\n' % (zamboni_tag, vendor_tag)
 
     log_file = os.path.join(settings.OUTPUT_DIR,
-                            re.sub('[^A-z0-9]', '.', zamboni_tag)) 
+                            re.sub('[^A-z0-9]', '.', zamboni_tag))
     output = open(log_file, 'a')
     run('pre_update:%s,%s' % (zamboni_tag, vendor_tag), output)
     pub('PUSH')
