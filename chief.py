ada582724f7880ca2dd97dc5bf2057d26a052065
Jeremy Orem <jeremy.orem@gmail.com>
2011-10-19 19:23:01 -0700

    Allow 1 chief for multiple apps.

--- chief.py
+++ chief.py
@@ -4,36 +4,39 @@ import re
 import subprocess
 
 import redis as redislib
-from flask import Flask, Response, request, render_template
+from flask import Flask, Response, abort, request, render_template
 
 import settings
 
 
 app = Flask(__name__)
-deploy = os.path.join(settings.ZAMBONI_DIR, 'scripts/update/update.py')
 
 os.environ['PYTHONUNBUFFERED'] = 'go time'
 
 
-def run(task, output):
-    subprocess.check_call('commander %s %s' % (deploy, task),
-                          shell=True, stdout=output, stderr=output)
+def do_update(app_name, app_settings, webapp_tag, who):
+    deploy = os.path.join(app_settings['dir'], 'scripts/update/update.py')
+    log_dir = os.path.join(settings.OUTPUT_DIR, app_name)
+    if not os.path.isdir(log_dir):
+        os.mkdir(log_dir)
 
+    def run(task, output):
+        subprocess.check_call('commander %s %s' % (deploy, task),
+                              shell=True, stdout=output, stderr=output)
 
-def do_update(zamboni_tag, who):
     def pub(event):
         redis = redislib.Redis(**settings.REDIS_BACKENDS['master'])
-        d = {'event': event, 'zamboni': zamboni_tag, 'who': who}
-        redis.publish(settings.PUBSUB_CHANNEL, json.dumps(d))
+        d = {'event': event, 'zamboni': webapp_tag, 'who': who}
+        redis.publish(app_settings['pubsub_channel'], json.dumps(d))
 
     try:
         pub('BEGIN')
-        yield 'Updating! zamboni: %s\n' % zamboni_tag
+        yield 'Updating! zamboni: %s\n' % webapp_tag
 
-        log_file = os.path.join(settings.OUTPUT_DIR,
-                                re.sub('[^A-z0-9]', '.', zamboni_tag))
+        log_file = os.path.join(log_dir,
+                                re.sub('[^A-z0-9]', '.', webapp_tag))
         output = open(log_file, 'a')
-        run('pre_update:%s' % zamboni_tag, output)
+        run('pre_update:%s' % webapp_tag, output)
         pub('PUSH')
         yield 'We have the new code!\n'
 
@@ -49,13 +52,19 @@ def do_update(zamboni_tag, who):
         raise
 
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
+@app.route("/<webapp>", methods=['GET', 'POST'])
+def index(webapp):
+    if webapp not in settings.WEBAPPS.keys():
+        abort(404)
+    else:
+        app_settings = settings.WEBAPPS[webapp]
+
     if request.method == 'POST':
         post = request.form
-        assert sorted(post.keys()) == ['password', 'who', 'zamboni']
-        assert post['password'] == settings.PASSWORD
-        return Response(do_update(post['zamboni'], post['who']),
+        assert sorted(post.keys()) == ['password', 'who', 'tag']
+        assert post['password'] == app_settings['password']
+        return Response(do_update(webapp, app_settings,
+                                  post['tag'], post['who']),
                         direct_passthrough=True,
                         mimetype='text/plain')
 
